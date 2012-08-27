bf540d782c7e14add6bfc5a63db333c93abad069
jugyo <jugyo.org@gmail.com>
2012-08-27 16:59:19 +0900

    fix tempalte keywords

--- {{{name}}}.py
+++ {{{name}}}.py
@@ -1,5 +1,5 @@
 import sublime, sublime_plugin
 
-class {{{class}}}Command(sublime_plugin.TextCommand):
+class {{{name}}}Command(sublime_plugin.TextCommand):
   def run(self, edit):
     sublime.message_dialog("foo")
