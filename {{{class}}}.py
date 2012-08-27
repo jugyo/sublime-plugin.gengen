e0f3776cf71cd3bad99c350286816ac53a6760c8
jugyo <jugyo.org@gmail.com>
2012-08-27 16:21:33 +0900

    rename main lib file

--- {{{class}}}.py
+++ {{{class}}}.py
@@ -1,5 +1,5 @@
 import sublime, sublime_plugin
 
-class ExampleCommand(sublime_plugin.TextCommand):
+class {{{class}}}Command(sublime_plugin.TextCommand):
   def run(self, edit):
     sublime.message_dialog("foo")
