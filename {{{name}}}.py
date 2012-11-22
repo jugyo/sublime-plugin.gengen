666da43b9bc3cec5126cf2c6b4986b0df8980f2c
jugyo <kkohno@gmail.com>
2012-11-23 00:45:12 +0900

    change the indent spaces

--- {{{name}}}.py
+++ {{{name}}}.py
@@ -1,5 +1,5 @@
 import sublime, sublime_plugin
 
 class {{{name}}}Command(sublime_plugin.TextCommand):
-  def run(self, edit):
-    sublime.message_dialog("foo")
+    def run(self, edit):
+        sublime.message_dialog("foo")
